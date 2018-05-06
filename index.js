99979f177460799de89522a9c0318197bb9ea9bb
Gonza Escandarani <gonziesc@hotmail.com>
2018-05-05 23:18:08 -0300

    final

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var C = 30;
+var C = 45;
 var NS = [0,0,0,0,0,0,0,0,0,0,0,0];
 var NT = [0,0,0,0,0,0,0,0,0,0,0,0];
 var T=0;
-var TF=599;
+var TF=50000;
 var TPLL = [0,0,0,0,0,0,0,0,0,0,0,0]
 var TPS = [Infinity, Infinity,Infinity,Infinity,Infinity,Infinity,Infinity,
 Infinity,Infinity,Infinity,Infinity,Infinity];
@@ -23,45 +23,45 @@ function buscarMenor(array,n){
 function rutinaIA(i) {
   switch(i) {
     case 0: {
-       return 1 + 24 * Math.random();
+       return 10 + 22 * Math.random();
     }
     case 1: {
-       return 7.333 * Math.random();
+       return 17 + 7.333 * Math.random();
     }
     case 2: {
-       return 12.333 * Math.random();
+       return 15 + 12.333 * Math.random();
     }
     case 3: {
-       return 20.666 * Math.random();
+       return 10 + 20.666 * Math.random();
     }
     case 4: {
-       return 20.666 * Math.random();
+       return 11 + 20.666 * Math.random();
     }
     case 5: {
-       return 9 * Math.random();
+       return 15 + 11 * Math.random();
     }
     case 6: {
-       return 25 * Math.random();
+       return 10 + 20 * Math.random();
     }
     case 7: {
-       return 32.33 * Math.random();
+       return 5 + 32.33 * Math.random();
     }
     case 8: {
-       return 5 * Math.random();
+       return 18 + 5 * Math.random();
     }
     case 9: {
-       return 15.6666 * Math.random();
+       return 13 + 15.6666 * Math.random();
     }
     case 10: {
-       return 33.333 * Math.random();
+       return 4 + 33.333 * Math.random();
     }
     case 11: {
-       return 0.5 + 33.333 * Math.random();
+       return 4 + 33.333 * Math.random();
     }
   }
 }
 function TA() {
-  return 5 + C * Math.random();
+  return 5 + (C - 5) * Math.random();
 }
 function Simular(){
   var i=buscarMenor(TPLL,12);
@@ -71,7 +71,7 @@ function Simular(){
       IA = rutinaIA(i);
       TPLL[i]= T + IA;
       NS[i]++;
-      if(NS[i] == 1){
+      if(NS[i] === 1){
         var ta = TA(C);
         TPS[i]= T + ta;
         STA[i]+=ta;
@@ -97,25 +97,30 @@ function Simular(){
     }
     else
     {
-      if((NS[0] >0 || NS[1] >0 || NS[2] >0 || NS[3] >0 || NS[4] >0 || NS[6] >0 || NS[6] >0 || NS[7] >0 || NS[8] >0 || NS[9] >0 || NS[10] >0 || NS[11] >0)){
+      if((NS[0] >0 || NS[1] >0 || NS[2] >0 || NS[3] >0 || NS[4] >0 || NS[5] >0 || NS[6] >0 || NS[7] >0 || NS[8] >0 || NS[9] >0 || NS[10] >0 || NS[11] >0)){
       for(k=0;k<12;k++){
         TPLL[k] = Infinity;
       }
         Simular();
       }
       else{
-        console.log("PPS CONSULTURIO: ", (SS[0] - STA[0] - SLL[0]) / NT[0]);
-        console.log("PPS CONTEO: ", (SS[1] - STA[1] - SLL[1]) / NT[1]);
-        console.log("PPS CVC: ", (SS[2] - STA[2] - SLL[2]) / NT[2]);
-        console.log("PPS ECOGRAFIA: ", (SS[3] - STA[3] - SLL[3]) / NT[3]);
-        console.log("PPS HRT: ", (SS[4] - STA[4] - SLL[4]) / NT[4]);
-        console.log("PPS NEURO: ", (SS[5] - STA[5] - SLL[5]) / NT[5]);
-        console.log("PPS OCT: ", (SS[6] - STA[6] - SLL[6]) / NT[6]);
-        console.log("PPS OIL MASTER: ", (SS[7] - STA[7] - SLL[7]) / NT[7]);
-        console.log("PPS RFG: ", (SS[8] - STA[8] - SLL[8]) / NT[8]);
-        console.log("PPS SPECTRALIS: ", (SS[9] - STA[9] - SLL[9]) / NT[9]);
-        console.log("PPS TODO PAQUI: ", (SS[10] - STA[10] - SLL[10]) / NT[10]);
-        console.log("PPS UBM: ", (SS[11] - STA[11] - SLL[11]) / NT[11]);       
+	console.log("Tiempo maximo de consulta:", C, " minutos");
+        console.log("PPS CONSULTURIO: ", Math.round(SS[0] - STA[0] - SLL[0]) / NT[0], " minutos");
+        console.log("PPS CONTEO: ", Math.round(SS[1] - STA[1] - SLL[1]) / NT[1], " minutos");
+        console.log("PPS CVC: ", Math.round(SS[2] - STA[2] - SLL[2]) / NT[2], " minutos");
+        console.log("PPS ECOGRAFIA: ", Math.round(SS[3] - STA[3] - SLL[3]) / NT[3], " minutos");
+        console.log("PPS HRT: ", Math.round(SS[4] - STA[4] - SLL[4]) / NT[4], " minutos");
+        console.log("PPS NEURO: ", Math.round(SS[5] - STA[5] - SLL[5]) / NT[5], " minutos");
+        console.log("PPS OCT: ", Math.round(SS[6] - STA[6] - SLL[6]) / NT[6], " minutos");
+        console.log("PPS OIL MASTER: ", Math.round(SS[7] - STA[7] - SLL[7]) / NT[7], " minutos");
+        console.log("PPS RFG: ", Math.round(SS[8] - STA[8] - SLL[8]) / NT[8], " minutos");
+        console.log("PPS SPECTRALIS: ", Math.round(SS[9] - STA[9] - SLL[9]) / NT[9], " minutos");
+        console.log("PPS TODO PAQUI: ", Math.round(SS[10] - STA[10] - SLL[10]) / NT[10], " minutos");
+        console.log("PPS UBM: ", Math.round(SS[11] - STA[11] - SLL[11]) / NT[11], " minutos");       
       }
     }
 }
+
+Simular();
+
+//  /bin/bash -c "ulimit -s 65500; exec /home/user/.nvm/versions/node/v8.9.4/bin/node --stack-size=65500 index.js"
